7d61eebadd51f7fa738673a2177266f4cfc03848
abhinav2o26 <abhinav2026jee@gmail.com>
2026-07-09 17:48:55 +0530

    Add files via upload

---
+++ Inputs-and-outputs-2/ans5.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char ch1,ch2;
+	cout<<"Enter the two characters ";
+	cin>>ch1>>ch2;
+	cout<<"Difference between ASCII values is "<<(int)ch1-(int)ch2;
+}
