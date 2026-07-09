b1d9e65f3ce1fa0463c557c0340a8aa3a03d86c8
abhinav2o26 <abhinav2026jee@gmail.com>
2026-07-09 17:55:21 +0530

    Delete Inputs-and-outputs-2 directory

--- Inputs-and-outputs-2/ans4.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-	int r,h;
-	cout<<"Enter the radius and height of cylinder ";
-	cin>>r>>h;
-	cout<<"The volume of cylinder is "<<3.1415*r*r*h;
-}
--- Inputs-and-outputs-2/ans5.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-	char ch1,ch2;
-	cout<<"Enter the two characters ";
-	cin>>ch1>>ch2;
-	cout<<"Difference between ASCII values is "<<(int)ch1-(int)ch2;
-}
